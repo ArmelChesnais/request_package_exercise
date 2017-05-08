edcbdc09f895d80b253926821399b58fdae60e80
Armel Chesnais <armel.chesnais@gmail.com>
2017-05-08 20:48:52 +0000

    implemented image download with update messages

--- request-package.js
+++ request-package.js
@@ -1,6 +1,26 @@
 var request = require('request');
+var fs = require('fs');
 
-request('https://sytantris.github.io/http-examples/', function(err, response, body) {
-  if (err) throw err;
-  console.log('Response Status Code:', response.statusCode);
-});
+var total = 0;
+
+request.get('https://sytantris.github.io/http-examples/future.jpg')               // Note 1
+       .on('error', function (err) {                                   // Note 2
+         throw err;
+       })
+       .on('response', function (response) {                           // Note 3
+         console.log('Response Status Code: ', response.statusCode,'\nContent type:', response.headers['content-type']);
+       })
+       .on('data', (data) => {
+          total += data.length;
+          console.log('Downloading file, ' + total + " bytes total");
+       })
+       .on('end', () => {
+          console.log("download complete!");
+       })
+       .pipe(fs.createWriteStream('./future.jpg'));               // Note 4
+
+// Notes:
+// 1. `request.get` is equivalent to `request()`
+// 2. `request.on('error', callback)` handles any error
+// 3. `request.on('response, callback)` handles the response
+// 4. What is happening here?
